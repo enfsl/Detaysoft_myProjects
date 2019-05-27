2e69771bce1f4bc2b534302fc187323852873c0c
jasyuiop <emrenefesli@outlook.com>
2019-05-27 18:00:05 +0300

    *

---
+++ Mouse_Yonunde_Donen_Ok/script.js
@@ -0,0 +1,35 @@
+function Create() {
+  Arrow();
+  ArrowEventListener();
+}
+
+function Arrow() {
+  var Container = document.getElementsByClassName("Box")[0];
+  var Arrow = document.createElement("img");
+  Arrow.src = "arrow.svg";
+  Arrow.id = "arrowsvg";
+  Container.appendChild(Arrow);
+}
+
+function ArrowEvent(event) {
+  var Arrow = document.getElementById("arrowsvg");
+  var offsetL = Arrow.offsetLeft; // ok'un sol konumunu aldım
+  var offsetT = Arrow.offsetTop; // üst konumu
+  var svgwidth = 200 / 2; // ok'un genişliğinin yarısını aldım
+  var svgheight = 100 / 2;
+  centerX = offsetL + svgwidth; // X eksenine göre ortasını buldum
+  centerY = offsetT + svgheight;
+  var arrowx = event.pageX;
+  var arrowy = event.pageY;
+  var radians = Math.atan2(arrowy - centerY, arrowx - centerX) // atan2 fonksiyonu radyan dönderiyor.
+
+  var degrees = radians * 180 / Math.PI; // 1 rad = 180/pi
+  Arrow.style.transform = "rotate(" + degrees + "deg)";
+}
+
+function ArrowEventListener() {
+  var Box = document.getElementsByClassName("Box")[0];
+  Box.addEventListener("mousemove", ArrowEvent);
+  // Box.addEventListener("mouseenter", ArrowEvent, false);
+  // Box.addEventListener("mouseleave", ArrowEvent, false);
+}
